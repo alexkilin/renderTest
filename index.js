76c8d2d1eec7d55baaefba3adee51476bc2d0f18
alexkilin <49560127+alexkilin@users.noreply.github.com>
2024-02-11 20:56:18 +0300

    Add files via upload
    part3 ready

--- index.js
+++ index.js
@@ -1,151 +1,119 @@
-// const http = require('http')
-//
+//ex 3.1-3.22
 
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const math = require('mathjs')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
+const mongoose = require('mongoose')
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    console.log('catch error......')
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :response-time :body'))
 app.use(express.static('dist'))
-
-let persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-
+app.use(requestLogger)
 
 
 morgan.token('body', (res) => { //creating body token
-let bodyTemp=res.body
-console.log(bodyTemp.toString)
-return bodyTemp
+    let bodyTemp = res.body
+    console.log(bodyTemp.toString)
+    return bodyTemp
 })
 
 app.get('/info', (request, response) => {
-console.log(persons.length)
-let date = new Date
-  response.send('<h3>Phonebook has info for ' + persons.length + ' people</h3><br/><h3> ' + new Date().toGMTString() + '</h3>  ')
+    console.log(persons.length)
+    let date = new Date
+    response.send('<h3>Phonebook has info for ' + persons.length + ' people</h3><br/><h3> ' + new Date().toGMTString() + '</h3>  ')
 })
 
-// app.get('/api/notes', (request, response) => {
-//   response.json(notes)
-// })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//   const id =Number(request.params.id)
-//   console.log(id)
-//   const note = notes.find(note => note.id === id)
-//   if (note) {
-//   response.json(note)
-// } else {
-//   response.status(404).end()
-// }
-// })
-
-app.get('/api/persons/:id', (request, response) => {
-  const id =Number(request.params.id)
-  console.log(id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-  response.json(person)
-} else {
-  response.status(404).end()
-}
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch (error => next(error))
 })
-
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
 
 const generateId = () => {
-  return math.randomInt(10000)
+    return math.randomInt(10000)
 }
-  app.post('/api/persons', (request, response) => {
+
+
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    console.log(request.body)
-    if ((!body.name)||(!body.number)) {
-      return response.status(400).json({
-        error: 'The name or number is missing'
-      })
-    }
 
-    const isNumberAlreadyExistInPhonebook = (body) => {
-      let result=false
-      persons.forEach((item, i) => {
-        if (item.number == body.number) {
-          result = true
-        }
-      });
-      return result
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
 
-    if (isNumberAlreadyExistInPhonebook(body)) {
-      return response.status(400).json({
-        error: 'The name already exists in the phonebook'
-      })
-    }
-
-
-
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   notes = notes.filter(note => note.id !== id)
-//
-//   response.status(204).end()
-// })
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
 
-const PORT = process.env.PORT || 3001
+    Person.findByIdAndUpdate(
+        request.params.id,
+        {name, number},
+        {new: true, runValidators: true, context: 'query'}
+    )
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
